d154783f74b605b5b2e7430a7576967dc6d7a0e5
Neil Kandalgaonkar <neilk@brevity.org>
2011-05-25 20:15:45 +0200

    initial sketch -- no persistence, history, or concurrency

---
+++ luna.py
@@ -0,0 +1,172 @@
+# makes unbound methods act like class methods
+class Callable:
+    	def __init__(self, anycallable):
+        	self.__call__ = anycallable
+
+class Util:
+	def removeFromList(x, item):
+		i = 0
+		while i < len(x):
+			if x[i] is item:
+				del x[i]
+			else:
+				i += 1
+	removeFromList = Callable(removeFromList)
+
+
+class Org:
+	def __init__(self, name):
+		self.name = name
+		self.roster = []
+		self.unproxiedUsers = []
+
+	def addUser(self, user):
+		self.roster.append(user)
+		self.addToUnproxiedUsers(user)
+
+	def removeFromUnproxiedUsers(self, user):
+		Util.removeFromList(self.unproxiedUsers, user)
+
+	def addToUnproxiedUsers(self, user):
+		self.unproxiedUsers.append(user)
+
+	def getUnproxiedUsers(self):
+		return self.unproxiedUsers
+		
+	def printTree(self, indent=0 ):
+		for user in self.unproxiedUsers:
+			user.debugSubtree()
+ 
+
+
+class User:
+	def __init__(self, name, org):
+		self.org = org
+		self.name = name
+		self.weight = 1
+		self.proxy = None
+		self.proxyChildren = []
+		self.votes = {} 
+		org.addUser(self)
+
+	def makeProxy(self, user):
+		org.removeFromUnproxiedUsers(self)
+		self.proxy = user
+		user.addProxyChild(self)
+
+	def removeProxy(self):
+		if self.proxy:
+			self.proxy.removeProxyChild(self)
+			self.proxy = None;
+			self.org.addToUnproxiedUsers(self)
+
+	def addProxyChild(self, user):
+		if user not in self.proxyChildren:
+			self.proxyChildren.append(user)
+
+	def removeProxyChild(self, user):
+		Util.removeFromList(self.proxyChildren, user)
+			
+	def vote(self, question, option):
+		self.votes[question] = option
+
+	# error cond if currently proxied?
+	def getVote(self, question):
+		vote = None
+		if self.votes.has_key(question):
+			vote = self.votes[question]
+		return vote
+
+	# this will benefit from caching -- a change of proxy should simply invalidate cache
+	# or, perhaps we will actually communicate weight upwards instantly... who knows
+	def getWeight(self):
+		weight = self.weight
+		for child in self.proxyChildren:
+			weight += child.getWeight()
+		return weight
+			
+		
+	def debugSubtree(self, indent=0 ):
+		print (' ' * indent) + self.name	
+		for user in self.proxyChildren:
+			user.debugSubtree(indent + 1)
+ 
+
+
+class Question:
+	def __init__(self,  org, text, options ):
+		self.org = org
+		self.text = text
+		self.options = options
+
+	def decide(self):
+		tally = Tally(self)
+		for user in self.org.getUnproxiedUsers():
+			weight = user.getWeight()
+			vote = user.getVote(self)
+			# is the decision in the available options for this question
+			tally.add(weight, vote)
+		winners = tally.getWinners()
+		if len(winners) == 0:
+			print "No winner?"
+		elif len(winners) > 1:
+			print 'Tie! ' + ', '.join( winners )
+		elif len(winners) == 1:
+			print 'Winner! ' + winners[0]
+		else:
+			# impossible?
+			print "Unknown result?!"			
+
+
+class Tally:
+	unknownDecision = '?'
+
+	def __init__(self, question):
+		self.counts = {}
+		self.counts[self.unknownDecision] = 0;
+		for option in question.options:
+			self.counts[option] = 0;
+		
+	
+	def add(self, weight, vote):
+		if vote in self.counts:
+			voteToCount = vote
+		else:
+			voteToCount = self.unknownDecision
+
+		self.counts[voteToCount] += weight
+
+
+	def getWinners(self):
+		max = 0
+ 		winners = []
+		for option in self.counts.keys():
+			print option + ':' + str(self.counts[option])
+			if self.counts[option] > max:
+				winners = [option]
+				max = self.counts[option]
+			elif self.counts[option] == max:
+				winners.append(option)
+		return winners
+			
+	
+if __name__ == '__main__':
+	org = Org('arsonists')
+	nicole = User('nicole', org)
+	ian = User('ian', org)
+	burstein = User('burstein', org)
+	ed = User('ed', org)
+	heather = User('heather', org)
+	
+	lunch = Question(org, 'lunch', [ 'thai', 'innout', 'pizza' ])
+
+	burstein.vote( lunch, 'thai' )
+	ian.vote( lunch, 'thai' )
+	nicole.vote( lunch, 'pizza' )
+	ed.makeProxy(nicole)
+	heather.makeProxy(nicole)
+	heather.removeProxy()
+
+	lunch.decide()
+	
+	org.printTree()	
