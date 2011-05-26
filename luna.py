432d304b54545938e3f194ef1b993b142e2818db
Neil Kandalgaonkar <neilk@brevity.org>
2011-05-26 11:27:34 +0200

    adding/removing users with time

--- luna.py
+++ luna.py
@@ -1,3 +1,5 @@
+import time
+
 # makes unbound methods act like class methods
 class Callable:
     	def __init__(self, anycallable):
@@ -17,12 +19,19 @@ class Util:
 class Org:
 	def __init__(self, name):
 		self.name = name
-		self.roster = []
+		self.membership = Membership()
 		self.unproxiedUsers = []
 
-	def addUser(self, user):
-		self.roster.append(user)
+	def addUser(self, time, user):
+		self.membership.join(time, user)
 		self.addToUnproxiedUsers(user)
+
+	def removeUser(self, time, user):
+		for proxiedUser in user.proxyChildren:
+			# XXX notify
+			proxiedUser.removeProxy()
+		self.membership.leave(time, user)
+		self.removeFromUnproxiedUsers(user)
 
 	def removeFromUnproxiedUsers(self, user):
 		Util.removeFromList(self.unproxiedUsers, user)
@@ -36,18 +45,65 @@ class Org:
 	def printTree(self, indent=0 ):
 		for user in self.unproxiedUsers:
 			user.debugSubtree()
- 
+
+
+class Membership:
+	def __init__(self):
+		self.roster = {};
+
+	def isOrWasMember(self, user):
+		return self.roster.has_key(user.id) 
+
+	def isMember(self, time, user):
+		isMember = False
+		if self.roster.has_key(user.id):
+			for event in self.roster[user.id]:
+				eventTime, eventType = event
+				if eventTime > time:
+					break
+				if eventType == 'begin':
+					isMember = True
+				elif eventType == 'end':
+					isMember = False
+				else:
+					throw (Error,e)
+		return isMember			
+
+	def join(self, time, user):
+		if not self.isMember(time, user):
+			if not self.roster.has_key(user.id):
+				self.roster[user.id] = []	
+			history = self.roster[user.id]
+			history.append((time, 'begin'))
+			# sort history by time before serializing		
+	
+	# throw error is not currently a member?
+	def leave(self, time, user):
+		if self.isMember(time, user):
+			history = self.roster[user.id]
+			history.append((time, 'end'))
+			# sort roster by time before serializing		
 
 
 class User:
-	def __init__(self, name, org):
+	maxId = None
+
+	def __init__(self, time, name, org):
 		self.org = org
+		self.id = self.incrementId()
 		self.name = name
 		self.weight = 1
 		self.proxy = None
 		self.proxyChildren = []
 		self.votes = {} 
-		org.addUser(self)
+		org.addUser(time, self)
+
+	def incrementId(self):
+		if self.maxId is None: 
+			self.maxId = 1
+		else:
+			self.maxId += 1
+		return self.maxId
 
 	def makeProxy(self, user):
 		org.removeFromUnproxiedUsers(self)
@@ -150,13 +206,15 @@ class Tally:
 		return winners
 			
 	
-if __name__ == '__main__':
+if __name__ == '__main__': 
+	time = time.time()
 	org = Org('arsonists')
-	nicole = User('nicole', org)
-	ian = User('ian', org)
-	burstein = User('burstein', org)
-	ed = User('ed', org)
-	heather = User('heather', org)
+	nicole = User(time, 'nicole', org)
+	ian = User(time, 'ian', org)
+	burstein = User(time, 'burstein', org)
+	ed = User(time, 'ed', org)
+	heather = User(time, 'heather', org)
+	alice = User(time, 'alice', org)
 	
 	lunch = Question(org, 'lunch', [ 'thai', 'innout', 'pizza' ])
 
@@ -166,6 +224,9 @@ if __name__ == '__main__':
 	ed.makeProxy(nicole)
 	heather.makeProxy(nicole)
 	heather.removeProxy()
+	alice.makeProxy(ed)
+	org.removeUser(time, nicole)
+	ed.vote(lunch, 'innout')
 
 	lunch.decide()
 	
