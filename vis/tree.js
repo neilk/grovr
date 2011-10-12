773bed9e7a39c342c6b99eafebdee27c411c03d2
Neil Kandalgaonkar <neilk@brevity.org>
2011-10-12 03:19:42 -0700

    made Org object, added proxies after the fact, displayed

--- vis/tree.js
+++ vis/tree.js
@@ -1,8 +1,13 @@
 var r = 960 / 2;
 
+var org = undefined; // defined below
+
 var tree = d3.layout.tree()
     .size([360, r - 120])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; })
+    .children( function(member) { 
+        return org.getProxyChildren(member);
+    } );
 
 var diagonal = d3.svg.diagonal.radial()
     .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
@@ -13,20 +18,22 @@ var vis = d3.select("#chart").append("svg:svg")
   .append("svg:g")
     .attr("transform", "translate(" + r + "," + r + ")");
 
-function loadJson(json) {
-  var nodes = tree.nodes(json);
+function update() {
+
+  // member0 is the root
+  var nodes = tree.nodes(org.getRoot());
 
   var link = vis.selectAll("path.link")
-      .data(tree.links(nodes))
+      .data(tree.links(nodes))   // <-- data is added here too
     .enter().append("svg:path")
       .attr("class", "link")
       .attr("d", diagonal);
 
   var node = vis.selectAll("g.node")
-      .data(nodes)
+      .data(nodes)    // <---- this is where the data is added
     .enter().append("svg:g")
       .attr("class", "node")
-      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+      .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
 
   node.append("svg:circle")
       .attr("r", 4.5);
@@ -39,4 +46,19 @@ function loadJson(json) {
       .text(function(d) { return d.name; });
 }
 
-loadJson( window.flare );
+// this is from flare.js
+org = generateRandomOrg( window.names, 10 );
+var proxies = [ [ '2', '4' ], [ '3', '4' ], [ '4', '6' ] ];
+function doProxies() {
+  console.log( 'doProxies' );
+  if ( proxies.length ) {
+    var proxy = proxies.shift();
+    org.setProxy( org.roster[proxy[0]], org.roster[proxy[1]] );
+    doProxies(); // window.setTimeout( doProxies, 1000 );
+  }
+}
+doProxies();
+update();
+
+
+
